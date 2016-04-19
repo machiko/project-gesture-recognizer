a3af45395cb932c851749eb19733cd44b231d281
reyes <machikoreo@gmail.com>
2016-04-19 09:15:10 +0800

    initial

--- product-gesture recognizer/ViewController.swift
+++ product-gesture recognizer/ViewController.swift
@@ -9,7 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    gesture recognizer
+    @IBAction func tapGesture(sender: AnyObject) {
+        addDigital()
+    }
+    
+    @IBAction func panGesture(sender: AnyObject) {
+        addDigital()
+    }
+//    宣告
+    @IBOutlet weak var counterTxt: UILabel!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    
+    @IBOutlet weak var resetBtn: UIButton!
+    
+    
+//    動作
+    @IBAction func addCounter(sender: AnyObject) {
+        addDigital()
+    }
+    
+    @IBAction func resetCounter(sender: AnyObject) {
+        counterTxt.text = String(0)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +45,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func addDigital() {
+        var tmpVal = Int(counterTxt.text!)
+        tmpVal = tmpVal! + 1
+        counterTxt.text = String(tmpVal!)
+    }
 
 }
 
